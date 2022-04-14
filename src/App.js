3135a197431b0ba2ceda7c00bf4c39bbd2a25b93
Joyal Thomas <joyal@neoito.com>
2022-04-15 01:04:03 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import edit_modal from './edit_modal'
+export default function App() {
+  return (
+    <div>
+      <edit_modal/>
+    </div>
+  )
+}
---
+++ src/edit_modal.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const edit_modal = () => {
+  return (
+   <p>Hello from edit_modal </p>
+  )
+}
+export default edit_modal
